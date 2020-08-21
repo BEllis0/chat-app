e9129c4c47e0edb5062c10de55292acd100e4230
Brandon Ellis <brandonellis.email@gmail.com>
2020-08-20 21:50:58 -0600

    db config

---
+++ database/config.js
@@ -0,0 +1,34 @@
+const { Pool, Client } = require('pg');
+
+const config = {
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: 'chat_app',
+  password: process.env.DB_PASS,
+  port: '5432',
+};
+
+module.exports.dbConnection = new Pool(config);
+
+module.exports.dbConnection.connect((err) => {
+    if (err) {
+        console.log(`Error connecting to db: ${err}`);
+    } else {
+        console.log('Successfully connected to database');
+    }
+});
+
+// handle errors in idle clients
+module.exports.dbConnection.on('error', (err, client) => {
+    if (err) {
+        console.log(`Error in idle client: ${err}`);
+    }
+});
+
+module.exports.dbConnection.on('acquire', (client) => {
+    console.log('Client checked out from the pool')
+});
+
+module.exports.dbConnection.on('remove', (client) => {
+    console.log('Client closed and removed');
+});
