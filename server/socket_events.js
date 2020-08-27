edb3de54da1e2b1c6b1bd9422fd47ce6893b103c
Brandon Ellis <brandonellis.email@gmail.com>
2020-08-26 20:48:56 -0600

    socket handlers for chat messages added; includes db models for adding messages and getting messages

--- server/socket_events.js
+++ server/socket_events.js
@@ -1,26 +1,42 @@
-const { addMessage } = require('./models/messages.js');
+const { addMessage, getAllMessages } = require('./models/messages.js'); 
 
 module.exports = {
     start: io => {
         // socket.io listening
         io.on('connection', (socket) => {
-            console.log('a user connected');
+            
+            // message for new user
+            // socket.emit('message', 'Welcome to chat');
 
+            socket.emit('chatMessage', 'test message')
+
+            // broadcast is shared with all connections other than new user
+            // socket.broadcast.emit('message', 'A user has joined the chat');
+            
             // user disconnects
             socket.on('disconnect', () => {
                 console.log('user disconnected');
+                // io.emit('message', 'A user has left the chat')
             });
 
             // user sends message
-            socket.on('message', (msg) => {
+            socket.on('message', async (msg) => {
                 
-                addMessage(msg)
+                await addMessage(msg)
                     .then(res => {
-                        console.log('Message Added')
+                        console.log('Message added to db');
                     })
                     .catch(err => {
                         console.log('Error adding message to db', err);
                     });
+                
+                await getAllMessages()
+                    .then(res => {
+                        io.emit('message', res.rows);
+                    })
+                    .catch(err => {
+                        console.log('Error getting messages in Socket.on message event', err);
+                    });  
             });
         });
     }
