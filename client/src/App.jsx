3d7c3d731e546871eaf1b4f62266dbe08e3720d5
Brandon Ellis <brandonellis.email@gmail.com>
2020-08-26 20:49:38 -0600

    created socket handlers to listen for messages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,34 +12,53 @@ class App extends React.Component {
         };
 
         //bindings
-        this.pushMessage = this.pushMessage.bind(this);
+        this.getAllMessages = this.getAllMessages.bind(this);
+    }
+
+    getAllMessages() {
+        fetch('http://localhost:3000/api/v1/messages/')
+        .then(response => {
+            return response.json();
+        })
+        .then(messages => {
+            console.log('Response of all messages in client', messages.rows);
+            this.setState({
+                messages: messages.rows
+            });
+        })
+        .catch(err => {
+            console.log('Error getting all messages in client', err);
+        });
     }
 
     componentDidMount() {
         // socket init
         window.socket = io();
+
+        // get all messages from db
+        this.getAllMessages();
+
+        socket.on('chatMessage', msg => {
+            console.log('message in component did mount', msg)
+            // this.getAllMessages();
+        });
+
+        socket.on('message', messages => {
+            this.setState({
+                messages: messages
+            }, () => console.log('messages state', this.state.messages))
+        });
+
     }
 
-    pushMessage(message) {
-        this.setState(currentState => {
-            const messages = currentState.messages.concat({
-                username: this.state.username,
-                message: message,
-                date: new Date()
-            });
-
-            return {
-                messages
-            }
-        }, () => console.log('state', this.state.messages))
-    }
 
     render() {
+        console.log('state in app comp', this.state.messages)
         return (
             <div className="appContainer">
                 <MessagesView
+                    getAllMessages={this.getAllMessages}
                     messages={this.state.messages}
-                    pushMessage={this.pushMessage} 
                 />
             </div>
         )
