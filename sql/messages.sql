7b53ce3a406d32031e4d3ff8a8d9041c90358b29
Brandon Ellis <brandonellis.email@gmail.com>
2020-08-29 00:33:03 -0600

    added username and chat room

--- sql/messages.sql
+++ sql/messages.sql
@@ -1,6 +1,8 @@
 CREATE TABLE chat_app.messages (
     id SERIAL PRIMARY KEY,
     _date TIMESTAMP default NOW(),
+    username VARCHAR(30),
+    room VARCHAR(30),
     sent_from INT,
     sent_to INT,
     message VARCHAR(140)
