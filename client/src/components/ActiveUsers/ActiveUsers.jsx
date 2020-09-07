61e057da1591ab0ffc49a82992e4b0d5aa52b7f2
Brandon Ellis <brandonellis.email@gmail.com>
2020-09-06 23:10:15 -0400

    listing active users

---
+++ client/src/components/ActiveUsers/ActiveUsers.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './activeUsers.scss';
+
+const ActiveUsers = props => {
+    console.log('active users comp', props)
+    return (
+        <div className="activeUsersContainer">
+            <h3>Active Users</h3>
+            {props.activeUsers.map(activeUser => {
+                return (
+                    <div key={props.activeUsers.indexOf(activeUser)}>
+                        <p className="activeUsername">{activeUser}</p>
+                    </div>
+                )
+            })}
+        </div>
+    )
+};
+
+export default ActiveUsers;
