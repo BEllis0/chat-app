91aad7a9a92bd64a4825fbf6451658a122116c7b
Brandon Ellis <brandonellis.email@gmail.com>
2020-09-06 23:14:10 -0400

    added sidebar component

--- client/src/components/Views/MessagesView/MessagesView.jsx
+++ client/src/components/Views/MessagesView/MessagesView.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import MessageForm from '../../Message_Form/MessageForm.jsx';
 import MessageList from '../../MessageList/MessageList.jsx';
+import Sidebar from '../../Sidebar/Sidebar.jsx';
+import MessagesHeader from '../../MessagesHeader/MessagesHeader.jsx';
+
+import styles from './messagesView.scss';
 
 const MessagesView = props => {
     return (
-        <div>
-            <MessageForm 
-                getAllMessages={props.getAllMessages}
-                handleMessageSubmit={props.handleMessageSubmit}
-            />
-            <MessageList 
-                messages={props.messages} 
-            />
+        <div className="messagesViewContainer">
+            
+            <MessagesHeader />
+            
+            <div className="flex">
+                <Sidebar
+                    activeUsers={props.activeUsers}
+                />
+                <div className="messagesModule">
+                    <MessageList 
+                        messages={props.messages} 
+                    />
+                    <MessageForm 
+                        getAllMessages={props.getAllMessages}
+                        handleMessageSubmit={props.handleMessageSubmit}
+                    />
+                </div>
+            </div>
         </div>
     )
 };
