0e3b6f917d49a42d849d34033b2170092edcbc3a
Brandon Ellis <brandonellis.email@gmail.com>
2020-09-20 20:23:17 -0600

    passing room props

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import styles from './sidebar.scss';
 import ActiveUsers from '../ActiveUsers/ActiveUsers.jsx';
+import Room from '../Room/Room.jsx';
 
 const Sidebar = (props) => {
     return (
         <div className="sidebarContainer">
+            <Room 
+                room={props.room}
+            />
             <ActiveUsers
                 activeUsers={props.activeUsers}
             />
